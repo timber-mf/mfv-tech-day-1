cb1a05dce1d545ec50a0da4b47dc7b6017bcc7a0
David <hungndl86@gmail.com>
2024-08-30 13:09:17 +0700

    add user entity

---
+++ backend/appt/src/main/kotlin/vn/mfv/booking/entity/User.kt
@@ -0,0 +1,17 @@
+package org.example.vn.mfv.booking.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+class User(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+    val googleId: String,
+    val name: String,
+    val email: String
+) {
+    constructor() : this(id = 0, googleId = "", name = "", email = "")
+}
